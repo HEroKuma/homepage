36dfa3b0b8b1930d24ac8afdaa1fae591693d313
HEroKuma <zx8733520+github@gapp.nthu.edu.tw>
2020-06-30 00:25:49 +0900

    add skill bar and icon

--- src/cv/header.js
+++ src/cv/header.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import data from './data.json';
+import icon from '../image/icon.png';
 
 const box = data.header.map((item) => {
     return (
         <div class="head">
-            <p class="name">{item.name}</p>
-            <p class="job">{item.job}</p>
+            <div class="head-box">
+                <p class="name">{item.name}</p>
+                <p class="job">{item.job}</p>
+            </div>
+            <div class="icon-box">
+                <img class="icon" src={icon}/>
+            </div>
         </div>
     )
 });
--- src/cv/skill.js
+++ src/cv/skill.js
@@ -3,9 +3,11 @@ import data from './data.json';
 import arrow from '../image/arrow-right.png';
 
 const skill_div = data.skill.map((item) => {
+    let bar = item.bar + "%";
     return (
         <div class="skill_box">
             <p class="skill_name"><span class="arrow">{item.name}</span>{item.year} years</p>
+            <div class="skill-bar" id={item.name}></div>
             <p>{item.skills}</p>
         </div>
     )
